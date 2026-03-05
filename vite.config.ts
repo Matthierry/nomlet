ea4c884081ac15135aa9dab2ed5c0298322c935c
Matthierry <Clarky-BWFC@hotmail.co.uk>
2026-03-05 10:12:20 +0000

    Add proper PWA icons

--- vite.config.ts
+++ vite.config.ts
@@ -19,14 +19,26 @@ export default defineConfig({
         scope: "/nomlet/",
         icons: [
           {
-            src: "/nomlet/images/nomlet-placeholder.png",
+            src: "/nomlet/icons/icon-192.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "/nomlet/images/nomlet-placeholder.png",
+            src: "/nomlet/icons/icon-512.png",
             sizes: "512x512",
             type: "image/png"
+          },
+          {
+            src: "/nomlet/icons/maskable-192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable"
+          },
+          {
+            src: "/nomlet/icons/maskable-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
           }
         ]
       }
